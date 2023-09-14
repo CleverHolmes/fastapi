f5f37677cf827db7d5e88fa83899f531c0267639
CleverHolmes <hugo.rogers.16@gmail.com>
2023-09-14 22:33:04 +0900

    update dict

--- main.py
+++ main.py
@@ -79,4 +79,8 @@ async def read_user_item(
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
