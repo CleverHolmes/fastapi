910fd5e6c8cdb433bb4986f9236d92bb28f5e1be
Kattlover99 <thrillseeker.nw@gmail.com>
2023-09-14 22:38:07 +0900

    update

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def create_item(item_id: int, item: Item, q: str | None = None):
     return result
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_items(q: str | None = Query(default=None, max_length=50)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
